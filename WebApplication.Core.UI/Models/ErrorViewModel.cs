943b42666f58ead936240b64591fd28e7236f1b0
Alan Macgowan <alanmacgowan@gmail.com>
2020-06-25 11:05:37 -0300

    created project

---
+++ WebApplication.Core.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication.Core.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
