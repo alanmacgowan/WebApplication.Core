b56399d4a4e5b054182d5b4eb5006ddad670ee2e
Alan Macgowan <alanmacgowan@gmail.com>
2020-09-18 22:49:51 -0300

    Added benchmarking project - initial commit

---
+++ WebApplication.Core.API/script.sql
@@ -0,0 +1,33 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200711034154_InitialCreate')
+BEGIN
+    CREATE TABLE [Employees] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NOT NULL,
+        [LastName] nvarchar(max) NOT NULL,
+        [BirthDate] datetime2 NULL,
+        [Country] nvarchar(max) NULL,
+        CONSTRAINT [PK_Employees] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20200711034154_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20200711034154_InitialCreate', N'3.1.5');
+END;
+
+GO
+
---
+++ WebApplication.Core.Benchmarking/Program.cs
@@ -0,0 +1,47 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+using System.Reflection;
+using System.Threading.Tasks;
+using WebApplication.Core.API.Controllers;
+using WebApplication.Core.Data;
+
+namespace WebApplication.Core.Benchmarking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+
+    public class EmployeeControllerTests
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                var value = Environment.GetEnvironmentVariable("WebApplication_ConnectionString");
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = (ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location)).AppSettings.Settings["WebApplication_ConnectionString"].Value;
+                }
+                return value;
+            }
+        }
+
+        [Benchmark]
+        public async Task GetEmployee() 
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<WebApplicationContext>();
+            optionsBuilder.UseSqlServer(ConnectionString);
+
+            var controller = new EmployeesController(new WebApplicationContext(optionsBuilder.Options));
+
+            await controller.GetEmployee(1);
+        }
+    }
+}
